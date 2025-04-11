86703eb8f81620e8b0aab55393af8e19d8f86629
Akaam Shamerany <alpharaohh@gmail.com>
2025-04-11 13:51:22 +0100

    Fix server file

--- src/server.tsx
+++ src/server.tsx
@@ -7,23 +7,23 @@ const port = 3000
 app.get("/", async (_, res) => {
   const app = (
     <Application
-    cacheStrategy= "force-cache"
-  description = "Application shell"
-  output = "An application with a sidebar with 3 items (home, data, settings). It should also have room for children {{Table}}"
+      cacheStrategy="force-cache"
+      description="Application shell"
+      output="An application with a sidebar with 3 items (home, data, settings). It should also have room for children {{Table}}"
     >
-    <Table
-      cacheStrategy="none"
-  description = "A table component"
-  output = "A table component with a header and 3 rows"
-    >
-    Hello, world!
-    </Table>
+      <Table
+        cacheStrategy="none"
+        description="A table component"
+        output="A table component with a header and 3 rows"
+      >
+        Hello, world!
+      </Table>
     </Application>
   )
 
-const htmlContent = await app.run()
+  const htmlContent = await app.run()
 
-res.send(htmlContent)
+  res.send(htmlContent)
 })
 
 app.listen(port, () => {
